70486e0aa40d05907c01d4faf447e5754558439b
bathsheba <petty.bathsheba@gmail.com>
2016-11-08 16:52:10 -0500

    commit new pizza app

---
+++ PizzaStoreMVC/PizzaStoreMVC.Client/DomainModels/Crust.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaStoreMVC.Client.DomainModels
+{
+    public class Crust
+    {
+    }
+}
---
+++ PizzaStoreMVC/PizzaStoreMVC.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PizzaStoreMVC.Client.MvcApplication" Language="C#" %>
